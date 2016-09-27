9ebff43cebe67978a2c74a7ef361212d7be8958a
Ask Solem <ask@celeryproject.org>
2016-09-27 11:25:49 -0700

    Bumps version to 1.1.0

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ classes = """
     Programming Language :: Python :: 2
     Programming Language :: Python :: 2.7
     Programming Language :: Python :: 3
-    Programming Language :: Python :: 3.3
     Programming Language :: Python :: 3.4
     Programming Language :: Python :: 3.5
     License :: OSI Approved :: BSD License
